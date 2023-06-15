93e30a236974a9d527b8abdcc8a3b9a44d729643
nompumel777 <p.kumalon@gmail.com>
2023-06-15 13:13:56 -0700

    Program using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing a line using the puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzl");
+return (0);
+}
