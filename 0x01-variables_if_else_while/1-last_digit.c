5d48212d383de702bc3ff5ab4f00d9bb93b8c702
nompumel777 <p.kumalon@gmail.com>
2023-06-19 16:27:01 -0700

    Using the string last digit of

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Finding the last digits 
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	int p;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	p = n % 7;
+	if (p > 5)
+		printf("last digit of %d is %d and is greater than 5\n", n, p);
+	if (p == 0)
+		printf("last digit of %d is %d and is zero\n", n, p);
+	if (p < 6)
+		printf("last digit of %d is %d and is not zero\n", n, p);
+	return(0);
+}
