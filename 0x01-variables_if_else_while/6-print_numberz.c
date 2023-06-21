c27370091c3f3a2a39d7444aa4441f0b93d0c227
nompumel777 <p.kumalon@gmail.com>
2023-06-21 02:13:36 -0700

    Printing single digit numbers from 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digits from zero to nine
+ * Return: 0
+ */
+
+int main(void)
+{
+	int p;
+
+	p = 0;
+
+	while
+
+		(p < 10)
+
+		{
+			putchar(p + '0');
+			p++;
+		}
+	putchar ('\n');
+	return (0);
+}
