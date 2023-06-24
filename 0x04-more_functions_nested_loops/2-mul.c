5858432b61e9c117d0548ce4e8585b5c64081ed0
nompumel777 <p.kumalon@gmail.com>
2023-06-24 07:28:45 -0700

    Function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - checks for a digit, 0 through 9
+ * @a: a digit to be checked
+ * @b: a digit to be checked
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
