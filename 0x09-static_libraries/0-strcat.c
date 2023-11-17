690964a97e6e718338f3a8f703ea36cdb1a4a076
Nompumelelo <p.kumalon@gmail.com>
2023-11-17 14:45:27 -0800

    Trying something out

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -4,7 +4,7 @@
  * @dest: input value
  * @src: input value
  *
- * Return: void	
+ * Return: void
  */
 
 char *_strcat(char *dest, char *src)
@@ -24,7 +24,7 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
+	}
 
 
 	dest[i] = '\0';
